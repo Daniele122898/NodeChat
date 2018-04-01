d0e626d1fab4bf514fad204f8df9015b49c68023
Serenity <d.palbanese@gmail.com>
2018-04-01 22:15:09 +0200

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
-const {generateMessage} = require("./utils/message");
+const {generateMessage, generateLocationMessage} = require("./utils/message");
 const publicPath = path.join(__dirname, "../public");
 const app = express();
 const server = http.createServer(app);
@@ -13,21 +13,25 @@ const io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on("connection", (socket)=>{
-   console.log("New user Connected");
+    console.log("New user Connected");
 
     socket.emit("newMessage", generateMessage("Admin", "Welcome to NodeChat"));
 
     socket.broadcast.emit("newMessage",generateMessage("Admin", "User joined the Chat!"));
 
-   socket.on("createMessage",(message, callback)=>{
+    socket.on("createMessage",(message, callback)=>{
       console.log("createMessage",message);
       io.emit("newMessage", generateMessage(message.from, message.text));
       callback("This is from the server");
-   });
+    });
 
-   socket.on("disconnect", ()=>{
+    socket.on("createLocationMessage", (coords)=>{
+       io.emit("newLocationMessage", generateLocationMessage("Admin", coords.latitude, coords.longitude))
+    });
+
+    socket.on("disconnect", ()=>{
        console.log("User Disconnected");
-   });
+    });
 });
 
 server.listen(port, ()=>{
