40134152b72156783a94bbd0734433ce190c1b1d
Serenity <d.palbanese@gmail.com>
2018-04-01 15:15:45 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on("connect", function () {
     console.log("Connected to server");
 
-    socket.emit("createMessage", {
-       from: "Dani12321312",
-       text: "Some other shit"
-    });
 });
 
 socket.on("disconnect", function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on("connection", (socket)=>{
    console.log("New user Connected");
 
-   socket.emit("newMessage",{
-      from: "DAni1231231",
-      text: "Hey wasuuuuuuuup",
-       createdAt: 123
-   });
-
    socket.on("createMessage",(message)=>{
       console.log("createMessage",message);
+      io.emit("newMessage", {
+         from: message.from,
+          text:message.text,
+          createdAt: new Date().getTime()
+      });
    });
 
    socket.on("disconnect", ()=>{
