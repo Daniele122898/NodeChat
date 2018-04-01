0f46d103ecda760e9e6a42665ab856c215fc50ec
Serenity <d.palbanese@gmail.com>
2018-04-01 15:52:06 +0200

    Added Greeting user and broadcast join of user

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on("connection", (socket)=>{
    console.log("New user Connected");
 
+    socket.emit("newMessage", {
+       from:"Admin",
+        text: "Welcome to NodeChat",
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit("newMessage",{
+       from:"Admin",
+        text:"User joined the Chat!",
+        createdAt: new Date().getTime()
+    });
+
    socket.on("createMessage",(message)=>{
       console.log("createMessage",message);
       io.emit("newMessage", {
